e0b21ab9589a707d1b4651c304ab2481aca4523d
Paul Juillard <Paul Juillard@169.254.150.236>
2018-02-25 19:37:24 +0100

    3.4GameBoy last sunday commit

--- src/ch/epfl/gameboj/GameBoy.java
+++ src/ch/epfl/gameboj/GameBoy.java
@@ -5,6 +5,11 @@
 
 package ch.epfl.gameboj;
 
+import ch.epfl.gameboj.component.memory.Ram;
+
 public class GameBoy {
 
+    private Bus bus = new Bus();
+    private Ram workRam = new Ram(8192);
+    private 
 }
