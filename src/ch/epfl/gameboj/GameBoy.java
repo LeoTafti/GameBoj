5548fcb64d01b64360666de46e27a15220cb8fe5
Paul Juillard <Paul Juillard@169.254.150.236>
2018-02-28 23:18:31 +0100

    3.4 gameboy update
    proposition attributs en commentaires

--- src/ch/epfl/gameboj/GameBoy.java
+++ src/ch/epfl/gameboj/GameBoy.java
@@ -9,33 +9,12 @@ import ch.epfl.gameboj.component.memory.Ram;
 import ch.epfl.gameboj.component.memory.RamController;
 
 public class GameBoy {
-
-//    private Bus bus;
-//    private Ram workRam;
-//    private Ram echoRam;
-//    private RamController workRamController;
-//    private RamController echoRamController;
-//    
-//    /**
-//     * Constructor
-//     * @param cartirdge
-//     */
-//    public GameBoy(Object cartirdge) {
-//        
-//        bus = new Bus();
-//        
-//        workRam = new Ram(8192);
-//        workRamController = new RamController(workRam, AddressMap.WORK_RAM_START, AddressMap.WORK_RAM_END);
-//        
-//        echoRam = new Ram(7680);
-//        echoRamController = new RamController(echoRam, AddressMap.ECHO_RAM_START, AddressMap.ECHO_RAM_END);
-//        
-//        bus.attach(workRam);
-//        bus.attach(echoRam);        
-//    }
-//    
     
     private Bus bus;
+//  private Ram workRam;
+//  private RamController workRamController;
+//  private RamController echoRamController;
+    
     
     /**
      * Constructor
