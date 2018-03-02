83746a851df79727b1d6499896e9e392d82c6a44
leotafti <leotafti@gmail.com>
2018-03-02 15:17:32 +0100

    Clocked
    Added javadoc

--- src/ch/epfl/gameboj/component/Clocked.java
+++ src/ch/epfl/gameboj/component/Clocked.java
@@ -1,5 +1,9 @@
 package ch.epfl.gameboj.component;
 
 public interface Clocked {
+    /**
+     * Asks component to execute operations it has to do during given cycle
+     * @param cycle the cycle index
+     */
     abstract void cycle(long cycle);
 }
