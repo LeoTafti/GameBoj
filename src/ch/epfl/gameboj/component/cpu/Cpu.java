1b406aaae2e91831dfb606cfda77966411af3f19
leotafti <leotafti@gmail.com>
2018-03-02 16:02:06 +0100

    Cpu
    Made some important fixes

--- src/ch/epfl/gameboj/component/cpu/Cpu.java
+++ src/ch/epfl/gameboj/component/cpu/Cpu.java
@@ -12,15 +12,12 @@ import ch.epfl.gameboj.component.Component;
 
 public final class Cpu implements Component, Clocked {
 
+    private enum Reg implements Register { A, F, B, C, D, E, H, L }
+    private enum Reg16 implements Register { AF, BC, DE, HL, PC, SP }
     
-    private enum Reg implements Register {
-        A, F, B, C, D, E, H, L
-      }
-    private enum Reg16 implements Register {
-        AF, BC, DE, HL, PC, SP
-    }
     private RegisterFile<Reg> registerFile = new RegisterFile<>(Reg.values());
     private RegisterFile<Reg16> register16File = new RegisterFile<>(Reg16.values());
+    
     private static final Opcode[] DIRECT_OPCODE_TABLE =
             buildOpcodeTable(Opcode.Kind.DIRECT);
    
@@ -37,58 +34,63 @@ public final class Cpu implements Component, Clocked {
         
     };
     
-    public void dispatch(Opcode  op) {
+    public void dispatch(int op) {
+        Opcode opcode = DIRECT_OPCODE_TABLE[op];
         
-    switch( op) {
-        case NOP: {
-        } break;
-        case LD_R8_HLR: {
-        } break;
-        case LD_A_HLRU: {
-        } break;
-        case LD_A_N8R: {
-        } break;
-        case LD_A_CR: {
-        } break;
-        case LD_A_N16R: {
-        } break;
-        case LD_A_BCR: {
-        } break;
-        case LD_A_DER: {
-        } break;
-        case LD_R8_N8: {
-        } break;
-        case LD_R16SP_N16: {
-        } break;
-        case POP_R16: {
-        } break;
-        case LD_HLR_R8: {
-        } break;
-        case LD_HLRU_A: {
-        } break;
-        case LD_N8R_A: {
-        } break;
-        case LD_CR_A: {
-        } break;
-        case LD_N16R_A: {
-        } break;
-        case LD_BCR_A: {
-        } break;
-        case LD_DER_A: {
-        } break;
-        case LD_HLR_N8: {
-        } break;
-        case LD_N16R_SP: {
-        } break;
-        case LD_R8_R8: {
-        } break;
-        case LD_SP_HL: {
-        } break;
-        case PUSH_R16: {
-        } break;
-     }
+        //TODO : implement each case
+        
+        switch(opcode.family) {
+            case NOP: {
+            } break;
+            case LD_R8_HLR: {
+            } break;
+            case LD_A_HLRU: {
+            } break;
+            case LD_A_N8R: {
+            } break;
+            case LD_A_CR: {
+            } break;
+            case LD_A_N16R: {
+            } break;
+            case LD_A_BCR: {
+            } break;
+            case LD_A_DER: {
+            } break;
+            case LD_R8_N8: {
+            } break;
+            case LD_R16SP_N16: {
+            } break;
+            case POP_R16: {
+            } break;
+            case LD_HLR_R8: {
+            } break;
+            case LD_HLRU_A: {
+            } break;
+            case LD_N8R_A: {
+            } break;
+            case LD_CR_A: {
+            } break;
+            case LD_N16R_A: {
+            } break;
+            case LD_BCR_A: {
+            } break;
+            case LD_DER_A: {
+            } break;
+            case LD_HLR_N8: {
+            } break;
+            case LD_N16R_SP: {
+            } break;
+            case LD_R8_R8: {
+            } break;
+            case LD_SP_HL: {
+            } break;
+            case PUSH_R16: {
+            } break;
+        }
+        
+        //TODO : update PC value, wait for x cycles, etc (cf guidlines 2.5.1.3)
     }
-    
+
     
     @Override
     public int read(int address) {
@@ -97,7 +99,6 @@ public final class Cpu implements Component, Clocked {
 
     @Override
     public void write(int address, int data) {
-        // TODO Auto-generated method stub
         
     }
 
@@ -107,20 +108,19 @@ public final class Cpu implements Component, Clocked {
     }
     
     /**
-     * creates table of the opcodes of given kind 
+     * Creates table of Opcodes of given kind 
+     *      indexed by opcodes encodings
      * @param kind DIRECT or PREFIXED
      * @return table of 256 opcodes
      */
-    static Opcode[] buildOpcodeTable(Opcode.Kind kind) {
+    private static Opcode[] buildOpcodeTable(Opcode.Kind kind) {
         
-        Opcode[] table = new Opcode[256]; //TODO TODO hardcoding 256
-        int i = 0;
+        Opcode[] table = new Opcode[256];
         for (Opcode o: Opcode.values()) {
-            if( o.kind == kind) {
-                table[i] = o;
-                i++;
+            if(o.kind == kind) {
+                table[o.encoding] = o;
             }
         }
-        return new Opcode[1];
+        return table;
     }
 }
