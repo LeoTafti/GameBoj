b371b0a0e27dcb71013081a9f065755c8bb03542
leotafti <leotafti@gmail.com>
2018-03-06 14:12:42 +0100

    CpuTest
    Modified CpuTest a little to make writting tests more efficient

--- test/ch/epfl/gameboj/component/cpu/CpuTest.java
+++ test/ch/epfl/gameboj/component/cpu/CpuTest.java
@@ -6,12 +6,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ch.epfl.gameboj.Bus;
+import ch.epfl.gameboj.Preconditions;
 import ch.epfl.gameboj.component.memory.Ram;
 import ch.epfl.gameboj.component.memory.RamController;
 
 class CpuTest {
     private Bus bus;
     private Cpu cpu;
+    
+    private final int RAM_SIZE = 20;
     
     @BeforeEach
     public void initialize() {
@@ -22,7 +25,7 @@ class CpuTest {
         
         cpu = new Cpu();
         bus = new Bus();
-        Ram ram = new Ram(20);
+        Ram ram = new Ram(RAM_SIZE);
         RamController ramController = new RamController(ram, 0);
         cpu.attachTo(bus);
         ramController.attachTo(bus);
@@ -34,14 +37,28 @@ class CpuTest {
         }
     }
     
+    private void writeAllBytes(int ... bytes) {
+        if(bytes.length > RAM_SIZE)
+            throw new IllegalArgumentException("not enough ram for that");
+        for(int i = 0; i<bytes.length; i++) {
+            int instr = bytes[i];
+            Preconditions.checkBits8(instr);
+            bus.write(i, instr);
+        }
+    }
+    
     
     @Test
     public void LD_HL_N16_isCorrectlyExecuted() {
         //Write 0x14 in HL
-        bus.write(0, Opcode.LD_HL_N16.encoding); //LD_HL_
-        bus.write(1, 0x4);
-        bus.write(2, 0x1);
-       
+//        bus.write(0, Opcode.LD_HL_N16.encoding); //LD_HL_
+//        bus.write(1, 0x4);
+//        bus.write(2, 0x1);
+//       
+        writeAllBytes(Opcode.LD_HL_N16.encoding,
+                0x4,
+                0x1);
+        
         cycleCpu(Opcode.LD_HL_N16.cycles);
 
         assertArrayEquals(new int[] {3, 0, 0, 0, 0, 0, 0, 0, 1, 4}, cpu._testGetPcSpAFBCDEHL());
