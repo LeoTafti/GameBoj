ece905e5044fe3718c5037c57257fa925ab79ce1
leotafti <leotafti@gmail.com>
2018-03-09 13:41:42 +0100

    Sig checks 4 & new opcode

--- src/ch/epfl/gameboj/component/cpu/Opcode.java
+++ src/ch/epfl/gameboj/component/cpu/Opcode.java
@@ -631,6 +631,7 @@ public enum Opcode {
     public final int totalBytes;
     public final int cycles, additionalCycles;
 
+    
     private Opcode(Kind kind, Family family, int encoding, int totalBytes, int cycles, int additionalCycles) {
         this.kind = kind;
         this.family = family;
