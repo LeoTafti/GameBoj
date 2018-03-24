c656594b6faa0260ebdac2221981b09c1f3ea5f2
leotafti <leotafti@gmail.com>
2018-03-24 12:26:28 +0100

    Timer : fixed errors

--- src/ch/epfl/gameboj/component/Timer.java
+++ src/ch/epfl/gameboj/component/Timer.java
@@ -19,9 +19,7 @@ public final class Timer implements Clocked, Component{
     
     private int DIV = 0, TIMA = 0, TMA = 0, TAC = 0;
     
-    private int[] tacValues = {9, 3, 5, 7};
-    
-    
+    private final int[] tacValues = {9, 3, 5, 7};
     
     
     /**
@@ -29,24 +27,20 @@ public final class Timer implements Clocked, Component{
      * @throws NullPointerException if cpu is null
      */
     public Timer(Cpu cpu) {
-        
-        cpu = Objects.requireNonNull(cpu);
-        
+        this.cpu = Objects.requireNonNull(cpu);
     }
     
     @Override
     public void cycle(long cycle) {
-        
         if(timerIsOn()) {
         
             boolean ps = state();
         
-            DIV= Bits.clip(16, DIV + 4);
+            DIV = Bits.clip(16, DIV + 4);
+            //TODO : what does "4" stand for ? Define a static final attribute
         
             incIfChange(ps);
-        
         }
-        
     }
 
     @Override
@@ -54,19 +48,14 @@ public final class Timer implements Clocked, Component{
         Preconditions.checkBits16(address);
 
         switch (address) {
-
         case AddressMap.REG_DIV:
             return DIV >> 8;
-
         case AddressMap.REG_TIMA:
             return TIMA;
-
         case AddressMap.REG_TMA:
             return TMA;
-
         case AddressMap.REG_TAC:
             return TAC;
-
         default:
             return NO_DATA;
         }
@@ -79,10 +68,17 @@ public final class Timer implements Clocked, Component{
         
         switch(address) {        
         case AddressMap.REG_DIV: {
-            boolean ps = state();
-            DIV = data << 8;
-            incIfChange(ps);}
-            break;               
+            boolean s0 = state();
+//            DIV = data << 8;
+            DIV = 0;
+            //TODO : remove
+            // Project guidelines say :
+            // "Notez que l'écriture d'une valeur quelconque
+            // à l'adresse 0xFF04 (ie. AddressMap.REG_DIV)
+            // provoque la remise à zéro de la totalité
+            // des bits du compteur principal."
+            incIfChange(s0);
+            }break;               
         case AddressMap.REG_TIMA:
             TIMA = data;
             break;               
@@ -90,15 +86,22 @@ public final class Timer implements Clocked, Component{
             TMA = data;
             break;               
         case AddressMap.REG_TAC: {
-            boolean ps = state();
+            boolean s0 = state();
             TAC = data;
-            incIfChange(ps);}
-            break;               
+            incIfChange(s0);
+            }break;               
         }            
         
     }
     
     /**
+     * TODO : add description
+     *      
+     *      + Can't we just say that this method
+     *      returns the index of bit of DIV to use, or smth like that ?
+     *      
+     *      "log2 of TIMA's increment rate" seems very unclear to me :)
+     *      
      * @return log2 of TIMA's increment rate
      */
     private int TIMA_setup() {
@@ -106,6 +109,7 @@ public final class Timer implements Clocked, Component{
     }
     
     /**
+     * Tests value of bit of index 2 of TAC
      * @return true if ON
      */
     private boolean timerIsOn() {
@@ -114,19 +118,26 @@ public final class Timer implements Clocked, Component{
     
     /**
      * State of timer
-     * @return true if ON and (TIMA-setup)-th bit is 1
+     * @return true if time is ON and (TIMA-setup)-th bit is 1
      */
     private boolean state() {
         return (timerIsOn() && Bits.test(DIV, TIMA_setup()));
     }
 
     /**
-     * increments TIMA, requests cpu interrupt if overflows, resets to TMA
+     * If previous state is true and current state is false increments TIMA.
+     * If this produces an overflow,
+     *      requests cpu interrupt and resets TIMA to TMA value
+     * @param previousState value of the previous state
      */
     private void incIfChange(boolean previousState) {
         
-        if(previousState != state()) {
-        
+//        if(previousState != state()) {
+        if((previousState == true) && (state() == false)) {
+            //TODO : remove
+            // Careful, previous if condition was true for
+            // previousState == false and state() == true
+            // (and it shouldn't be)
             TIMA += 1;
             
             if(TIMA > 0xFF) {
