c1104ccbbe6297c51dcd0f3d6bf385b5fd4be5a3
leotafti <leotafti@gmail.com>
2018-04-13 15:41:22 +0200

    BitVector

---
+++ src/ch/epfl/gameboj/bits/BitVector.java
@@ -0,0 +1,253 @@
+package ch.epfl.gameboj.bits;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import ch.epfl.gameboj.Preconditions;
+
+public final class BitVector {
+
+    private enum ExtractType {
+        ZERO_EXT, WRAPPED
+    };
+
+    private final int[] elements;
+
+    /**
+     * Constructs a BitVector of given size (in bits), all bits set to given
+     * default value (false for 0, true for 1)
+     * 
+     * @param size
+     *            size in bits
+     * @param defaultValue
+     *            initial value of all bits
+     * @throws IllegalArgumentException
+     *             if size is negative or size isn't a multiple of Integer.SIZE
+     *             (32)
+     */
+    public BitVector(int size, boolean defaultValue) {
+        Preconditions.checkArgument(size >= 0 && size % Integer.SIZE == 0);
+
+        // TODO : should we absolutely call the private constructor ?
+        // means we have define a method like "initializeArray" or something of
+        // the sort
+        elements = new int[size];
+
+        Arrays.fill(elements, defaultValue ? 1 : 0);
+    }
+
+    /**
+     * Constructs a BitVector of given size (in bits), all bits set by default
+     * to 0
+     * 
+     * @param size
+     *            size in bits
+     * @param defaultValue
+     *            initial value of all bits
+     * @throws IllegalArgumentException
+     *             if size is negative or size isn't a multiple of Integer.SIZE
+     *             (32)
+     */
+    public BitVector(int size) {
+        this(size, false);
+    }
+
+    /**
+     * Private constructor Constructs a BitVector from given bits array (no
+     * copy)
+     * 
+     * @param elements
+     *            bits of BitVector
+     */
+    private BitVector(int[] elements) {
+        this.elements = elements;
+    }
+
+    /**
+     * Builder for BitVector Allows to create a BitVector incrementally, byte by
+     * byte
+     */
+    public final static class Builder {
+        private int[] elements;
+
+        /**
+         * Creates a new active Builder
+         * 
+         * @param size
+         *            number of bits, must be 0mod32
+         */
+        public Builder(int size) {
+            Preconditions.checkArgument(size >= 0 && size % Integer.SIZE == 0);
+            elements = new int[size / Integer.SIZE];
+        }
+
+        /**
+         * Builds BitVector of previously given elements (0 as default)
+         * 
+         * @return
+         */
+        public BitVector build() {
+            if (elements == null)
+                throw new IllegalStateException("already built");
+            BitVector r = new BitVector(elements);
+            elements = null;
+            return r;
+
+        }
+
+        /**
+         * Sets byte at given index of pending BitVector
+         * 
+         * @param index
+         *            byte's LSB will be written at BitVetor's index
+         * @param b
+         *            byte value
+         * @return
+         */
+        public Builder setByte(int index, byte b) {
+            Objects.checkIndex(0, elements.length);
+            if (elements == null)
+                throw new IllegalStateException("already built");
+            else
+                elements[index / Integer.BYTES] += b << index * Byte.SIZE;
+            return this;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object thatO) {
+        return (thatO instanceof BitVector
+                && Arrays.equals(elements, ((BitVector) thatO).elements));
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(elements);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i : elements) {
+            sb.append(Integer.toBinaryString(i));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Getter on BitVector size in bits
+     * 
+     * @return size in bits
+     */
+    public int size() {
+        return elements.length * Integer.SIZE;
+    }
+
+    /**
+     * Tests if bit at given index is 1
+     * 
+     * @param index
+     *            index of bit to test
+     * @return true if 1, false if 0
+     * @throws IllegalArgumentException
+     *             if given index is negative, or greater or equal this size in
+     *             bits
+     */
+    public boolean testBit(int index) {
+        Preconditions.checkArgument(index >= 0 && index < size());
+        return Bits.test(elements[index / Integer.SIZE], index % Integer.SIZE);
+    }
+
+    /**
+     * Computes bitwise "and" of this BitVector and given BitVector
+     * 
+     * @param that
+     *            other BitVector
+     * @return bitwise "and", as a new BitVector
+     */
+    public BitVector and(BitVector that) {
+        int length = elements.length;
+        Preconditions.checkArgument(length == that.elements.length);
+
+        int[] res = new int[length];
+        for (int i = 0; i < length; i++)
+            res[i] = elements[i] & that.elements[i];
+        return new BitVector(res);
+    }
+
+    /**
+     * Computes bitwise "or" of this BitVector and given BitVector
+     * 
+     * @param that
+     *            other bitVector
+     * @return bitwise "or", as a new BitVector
+     */
+    public BitVector or(BitVector that) {
+        int length = elements.length;
+        Preconditions.checkArgument(length == that.elements.length);
+
+        int[] res = new int[length];
+        for (int i = 0; i < length; i++)
+            res[i] = elements[i] | that.elements[i];
+        return new BitVector(res);
+    }
+
+    /**
+     * Computes the generalized extraction of size-bits, in the zero-extended
+     * version ("extends" BitVector by adding as many 0's as necessary)
+     * 
+     * @param fromIndex
+     *            index of LSB to extract (inclusive)
+     * @param size
+     *            number of bits to extract
+     * @return new BitVector of extracted bits
+     */
+    public BitVector extractZeroExtended(int fromIndex, int size) {
+        return extract(fromIndex, size, ExtractType.ZERO_EXT);
+    }
+
+    /**
+     * Computes the generalized extraction of size-bits, in the wrapped version
+     * ("extends" BitVector by repeating its bits over and over)
+     * 
+     * @param fromIndex
+     *            index of LSB to extract (inclusive)
+     * @param size
+     *            number of bits to extract
+     * @return new BitVector of extracted bits
+     */
+    public BitVector extractWrapped(int fromIndex, int size) {
+        return extract(fromIndex, size, ExtractType.WRAPPED);
+    }
+
+    private BitVector extract(int fromIndex, int size, ExtractType type) {
+
+    }
+
+    /**
+     * Computes shifting by delta-bits
+     * 
+     * @param delta
+     *            number of bits to shift by (positive for a left shift,
+     *            negative for a right shift)
+     * @return new bitVector with shifted bits
+     */
+    public BitVector shift(int delta) {
+        return extract(-delta, Integer.SIZE, ExtractType.ZERO_EXT);
+    }
+}
