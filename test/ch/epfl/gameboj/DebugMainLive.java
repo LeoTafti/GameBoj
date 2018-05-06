55edeb2d9e1288500b227b66fea127bb93d497ae
Paul Juillard <Paul Juillard@169.254.150.236>
2018-05-06 22:52:39 +0200

    10 yay it works

--- test/ch/epfl/gameboj/DebugMainLive.java
+++ test/ch/epfl/gameboj/DebugMainLive.java
@@ -23,7 +23,7 @@ public final class DebugMainLive extends Application {
 
     /** Configuration */
 
-//    private static final String ROM_PATH = "C:/Users/Paul Juillard/Documents/EPFL/BA2/POO/Projet/GameBoj/test/ch/epfl/gameboj//sprite_priority.gb";
+//    private static final String ROM_PATH = "C:/Users/Paul Juillard/Documents/EPFL/BA2/POO/Projet/GameBoj/test/ch/epfl/gameboj/flappyBoy.gb";
     private static final String ROM_PATH = "/Users/Leo/git/GameBoj/test/ch/epfl/gameboj/tasmaniaStory.gb";
 
     private static final float EMULATION_SPEED = 2f;
