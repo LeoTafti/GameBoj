ce31d80922a0ecb8c468159c4bea086f219ab2f8
leotafti <leotafti@gmail.com>
2018-05-11 14:10:52 +0200

    main

--- src/ch/epfl/gameboj/gui/Main.java
+++ src/ch/epfl/gameboj/gui/Main.java
@@ -32,6 +32,7 @@ public class Main extends Application{
 //    private static final String ROM_PATH = "/Users/Leo/git/GameBoj/roms/Legend of Zelda, The - Link's Awakening.gb";
     private static final String ROM_PATH = "/Users/Leo/git/GameBoj/roms/Super Mario Land 2.gb";
     
+    
     public static void main(String[] args) {
        launch(args);
     }
