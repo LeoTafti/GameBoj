917abbaed2b289436b973b9bafb6c6dbf208a927
leotafti <leotafti@gmail.com>
2018-05-11 16:08:29 +0200

    main

--- src/ch/epfl/gameboj/gui/Main.java
+++ src/ch/epfl/gameboj/gui/Main.java
@@ -23,7 +23,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
 
-    private static final float SIM_SPEED = 4f;
+    private static final float SIM_SPEED = 1f;
     
 //    private static final String[] ROM_PATHS = { 
 //            "roms/Tetris.gb", //0
@@ -38,7 +38,7 @@ public class Main extends Application{
 //            "roms/LegendofZelda,TheLink'sAwakening.gb" }; //9
 //    private static final String ROM_PATH = ROM_PATHS[8];
 //    private static final String ROM_PATH = "/Users/Leo/git/GameBoj/roms/SuperMarioLand2.gb";
-    private static final String ROM_PATH = "/Users/Leo/git/GameBoj/roms/Bomberman.gb";
+    private static final String ROM_PATH = "/Users/Leo/git/GameBoj/roms/Tetris.gb";
     
     
     public static void main(String[] args) {
