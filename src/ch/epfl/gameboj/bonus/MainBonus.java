4cb94294f21ebd4acf2914b174e7a3e2d667add2
leotafti <leotafti@gmail.com>
2018-05-18 23:21:53 +0200

    mainbonus

--- src/ch/epfl/gameboj/bonus/MainBonus.java
+++ src/ch/epfl/gameboj/bonus/MainBonus.java
@@ -64,7 +64,7 @@ public class MainBonus extends Application{
             "roms/LegendofZelda,TheLink'sAwakening.gb", //9
             };
     
-    private static String romPath = ROM_PATHS[5];
+    private static String romPath = ROM_PATHS[8];
     
     public static GameBoy gameboj;
     
